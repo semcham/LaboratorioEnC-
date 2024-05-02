28068d50f4fe8d93bb169ff4007fe5e3e3e477b8
semcham <137580427+semcham@users.noreply.github.com>
2024-05-02 08:31:03 -0500

    cambios
    se genero nuevos cambios

---
+++ TareasDeEstrcuturaDeDatos/Tarea2DeEstructura/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class Vehiculo
+{
+    public decimal VelocidadMaxima { get; set; }
+    public int NumeroRuedas { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+
+    public Vehiculo(string marca, string modelo)
+    {
+        Marca = marca;
+        Modelo = modelo;
+    }
+}
+
+class Moto : Vehiculo
+{
+    public int Cilindrada { get; set; }
+
+    public Moto(string marca, string modelo, int cilindrada) : base(marca, modelo)
+    {
+        Cilindrada = cilindrada;
+    }
+    Vehiculo vehiculo = (Vehiculo)new Moto("ducati", "A1", 1000);
+}
+
